7129d6fbc6058b5dae1e20580498986be48b9e05
Azat Mardan <hi@azat.co>
2017-05-29 13:18:54 -0700

    add changes :bus:

--- code/status.js
+++ code/status.js
@@ -1,35 +1,35 @@
 var os = require('os'),
   exec = require('child_process').exec,
   async = require('async'),
-  started_at = new Date();
+  started_at = new Date()
 
 module.exports = function(req, res, next) {
-  var server = req.app;
+  var server = req.app
   if(req.param('info')) {
     var connections = {},
-      swap;
+      swap
     async.parallel([
       function(done) {
         exec('netstat -an | grep :80 | wc -l', function(e, res) {
-          connections['80'] = parseInt(res,10);
-          done();
-    }); },
+          connections['80'] = parseInt(res,10)
+          done()
+    }) },
       function(done) {
         exec(
           'netstat -an | grep :'
             + server.set('port')
             + ' | wc -l',
           function(e, res) {
-            connections[server.set('port')] = parseInt(res,10);
-            done();
-    } );
+            connections[server.set('port')] = parseInt(res,10)
+            done()
+    } )
       },
       function(done) {
         exec('vmstat -SM -s | grep "used swap" | sed -E "s/[^0-9]*([0-9]{1,8}).*/\1/"',
         function(e, res) {
-          swap = res;
-          done();
-         });
+          swap = res
+          done()
+         })
       }], function(e) {
         res.send({
           status: 'up',
@@ -49,10 +49,10 @@ module.exports = function(req, res, next) {
           hostname: os.hostname(),
           connections: connections,
           swap: swap
-          });
-      });
+          })
+      })
   }
   else {
-    res.send({status: 'up'});
+    res.send({status: 'up'})
   }
 }
